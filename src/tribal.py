36f5ad616e2c101dd362ea86b1e4c43a2b11799f
Leah Weber <leahlw2@ada.cs.illinois.edu>
2022-11-11 17:16:56 -0600

    cleaned up code

--- src/tribal.py
+++ src/tribal.py
@@ -7,6 +7,7 @@ import argparse
 import pandas as pd 
 import pickle
 from spr import SPR
+from unrooted_spr import USPR
 from copy import deepcopy
 from trans_matrix import TransMat
 import utils as ut
@@ -15,7 +16,7 @@ from tree_utils import TribalTree
 
 class Tribal:
     def __init__(self, alignment, root, isotype_labels, seed,  n_init=1, 
-                init="random", transmat=None, alpha=0.5, jump_prob=0.5, n_isotypes=7,
+                init="random", transmat=None, alpha=0.9, jump_prob=0.5, n_isotypes=7,
                 cost_function=None, alphabet= ("A", "C", "G", "T", "-")):
         
         self.alignment = alignment
@@ -65,14 +66,81 @@ class Tribal:
             seq  ="".join([i for i in alignment[n]])
             print(f"Node {n}: Seq: {seq}, isotype: {isotype_labels[n]}")
 
-        
+    @staticmethod
+    def prob_state_change(e_new, e, T):
+        return np.exp(-1*(e_new - e)/T)
+    
 
-    def hill_climbing(self, TTree):
+    def parsimony(self, ttree):
+        curr_pars, curr_iso, labels, isotypes = ttree.parsimony(
+                                self.alignment,
+                                self.isotype_labels,
+                                self.Q_isotype,
+                                self.alphabet, 
+                                self.cost_function )
+        curr_obj= self.alpha*(curr_pars) + (1-self.alpha)*(-1*curr_iso)
+        return curr_obj, curr_pars, curr_iso, labels, isotypes
+
+    def simulated_annealing(self, curr_state, restart, temp=50, k_max=50):
+    
+      
+
+        curr_obj, curr_pars, curr_iso, _, _ = self.parsimony(curr_state)
+      
+        best_obj = curr_obj
+        best_state = deepcopy(curr_state)
+
+        #initialize neighbors
+        neighbors = iter(USPR(curr_state.get_unrooted_tree(), self.rng))
+
+        for k in range(k_max):
+            if (curr_obj - best_obj)/best_obj >= 0.1:
+                curr_obj = best_obj 
+                curr_state = best_state
+            cand_state = deepcopy(curr_state)
+              
+            #get the next candidate spr tree 
+            try:
+                t = next(neighbors)
+            except:
+                # neighbors =iter(USPR(cand_state.get_unrooted_tree(), self.rng,min_radius=3, max_radius=3))
+                # try:
+                #     t = next(neighbors)
+                break
+
+            cand_state.set_unrooted_tree(t)
+            curr_temp = temp*np.power(0.99,k)
+
+            #compute the objective value
+            cand_obj, cand_pars, cand_iso, _, _ = self.parsimony(cand_state)
+            if k % 25 ==0:
+                print(f"{restart},{k},{cand_obj},{curr_obj},{best_obj}")     
+
+            #check if solution is better than current state or randomly jump to new state with probability P(cand_obj, curr_obj,curr_temp)
+            if cand_obj < curr_obj or  self.rng.random() <= self.prob_state_change(cand_obj, curr_obj, curr_temp ):
+       
+                curr_state = deepcopy(cand_state)
+                neighbors = iter(USPR(curr_state.get_unrooted_tree(), self.rng))
+                curr_obj = cand_obj
+                if curr_obj < best_obj:
+                    best_obj = curr_obj
+                    best_state = deepcopy(curr_state)
+                    
+           
+              
+        return best_obj, best_state
+
+
+        #compute the initial solution
+
+
+
+    def greedy_hill_climbing(self, TTree):
 
             iterations = 0
             best_trees = []
      
-            best_score, best_iso_score = TTree.parsimony(self.Q_isotype,self.alphabet, self.cost_function, )
+            best_score, best_iso_score = TTree.parsimony(self.Q_isotype,self.alphabet, self.cost_function )
             best_tree = deepcopy(TTree)
             best_trees.append(best_tree)
             # best_score = self.alpha*(best_score) + (1-self.alpha)*(best_iso_score)
@@ -98,23 +166,30 @@ class Tribal:
             while True:
                 iterations += 1
                 cand_tribal = deepcopy(best_tree)
+    
                
-                spr_trees = SPR(best_tree, self.Q_isotype)
+                # spr_trees = SPR(best_tree, self.Q_isotype)
+                spr_trees = USPR(cand_tribal.get_unrooted_tree())
                 for t in spr_trees:
+                    cand_tribal.set_unrooted_tree(t)
+                    
                     count += 1
                   
                     improvement = False
                     # print("best tree")
                     # print(best_tree)
-                    cand_tribal.set_tree(t)
+    
                     # print(cand_tribal)
-                    cand_score, iso_score =cand_tribal.parsimony(self.Q_isotype,self.alphabet, self.cost_function)
+                    cand_score, iso_score =cand_tribal.parsimony(self.Q_isotype,
+                                                                self.alphabet, 
+                                                                self.cost_function)
                  
                     lin_comb_score = self.alpha*(cand_score) + (1-self.alpha)*(-1*iso_score)
                     
                     if cand_score < best_score or (cand_score==best_score and iso_score > best_iso_score): #or (cand_score == best_score and cand_iso_score < best_iso_score):
 
                         # print(list(t.edges))
+                      
                         print(f"Candidate score: {cand_score} Best Score: {best_score} Cand Iso: {iso_score} Best Iso: {best_iso_score}")
                         improvement = True
                         best_score = cand_score
@@ -133,8 +208,8 @@ class Tribal:
                     print("Total Trees Examined: " + str(count))
                     break
                 
-                if iterations > 2:
-                    break
+                # if iterations > 2:
+                #     break
                     # if self.rng.random() < 0.99/iterations:
                     #     best_score = cand_score
                     #     best_tree = cand_tribal
@@ -159,92 +234,79 @@ class Tribal:
 
     #     return tree
 
-    def random_tree_old(self):
-        tree = nx.Graph()
-        ids = self.ids.copy()
-        n = len(ids)
-        center = 2*n -3 
-        for i in range(n):
-            tree.add_edge(i, center)
+    # def random_tree_old(self):
+    #     tree = nx.Graph()
+    #     ids = self.ids.copy()
+   
+    #     n = len(ids)
+    #     center = str(2*n -3 )
+    #     for i in ids:
+    #         tree.add_edge(i, center)
       
-        next_node = n
+    #     next_node = n
        
-        for i in range(n-3):
-            pair = self.rng.choice(ids,2, replace=False)
+    #     for i in range(n-3):
+    #         pair = self.rng.choice(ids,2, replace=False)
     
 
-            for p in pair:
-                tree.add_edge(p, next_node)
-                tree.remove_edge(p, center)
-                tree.add_edge(p, next_node)
-                tree.add_edge(center, next_node)
-                ids.remove(p)
-            ids.append(next_node)
-            next_node += 1
-        
+    #         for p in pair:
+    #             tree.add_edge(p, str(next_node))
+    #             tree.remove_edge(p, center)
+    #             tree.add_edge(center, str(next_node))
+    #             ids.remove(p)
+    #         ids.append(str(next_node))
+    #         next_node += 1
+          
+    #     return tree
 
-        tree= self.root_tree(tree, self.root)
-        # nx.set_node_attributes(tree, self.isotype_labels, self.ISO)
-        # nx.set_node_attributes(tree, self.alignment, self.SEQ)
-     
-    
+    # def random_tree(self):
+    #     tree = nx.Graph()
   
-        return tree
-
-    def random_tree(self):
-        tree = nx.Graph()
-  
-        ids = []
-        for s in range(self.n_isotypes):
-            for i in self.ids:
-                if self.isotype_labels[i] ==s:
-                    ids.append(i)
+    #     ids = []
+    #     for s in range(self.n_isotypes):
+    #         for i in self.ids:
+    #             if self.isotype_labels[i] ==s:
+    #                 ids.append(i)
         
-        n = len(ids)
-        center = 2*n -3 
-        for i in ids:
-            tree.add_edge(i, center)
+    #     n = len(ids)
+    #     center = 2*n -3 
+    #     for i in ids:
+    #         tree.add_edge(i, center)
       
-        next_node = n
+    #     next_node = n
        
-        for i in range(n-3):
-            pair = [ids[0], ids[1]]
+    #     for i in range(n-3):
+    #         pair = [ids[0], ids[1]]
     
 
-            for p in pair:
-                tree.add_edge(p, next_node)
-                tree.remove_edge(p, center)
-                tree.add_edge(center, next_node)
-                ids.remove(p)
-            ids.append(next_node)
-            next_node += 1
+    #         for p in pair:
+    #             tree.add_edge(p, next_node)
+    #             tree.remove_edge(p, center)
+    #             tree.add_edge(center, next_node)
+    #             ids.remove(p)
+    #         ids.append(next_node)
+    #         next_node += 1
         
-        # nx.set_node_attributes(tree, self.isotype_labels, self.ISO)
-        # nx.set_node_attributes(tree, self.alignment, self.SEQ)
-  
-        return tree
+    
+    #     return tree
 
-    def initialize_random(self):
+    def initialize_random(self, n_init):
 
-        iso_labs = {k: [v] for k,v in self.isotype_labels.items()}
-
-        tree = self.random_tree()
-        ttree =TribalTree(tree, root=self.root, is_rooted=False, 
-                        sequences=self.alignment.copy(), isotypes=iso_labs)
+        ttrees = []
+        for i in range(n_init):
+            ttrees.append(TribalTree(root=self.root, ids = self.ids.copy(), rng=self.rng))
         
-        return ttree
+        return ttrees
     
 
     def initialize_nj(self):
 
 
         tree = nj.neighbor_joining(self.dmat, self.ids)
-        iso_labs = {k: [v] for k,v in self.isotype_labels.items()}
-        ttree =TribalTree(tree, self.root, is_rooted=False , 
-                    sequences =self.alignment.copy(), 
-                    isotypes=iso_labs)
-
-        return ttree
+        ttree =TribalTree(tree, self.root, is_rooted=False)
+               
+        #TODO: initialize with random perturbation of NJ tree
+        return [ttree]
 
     # def initialize_edmonds(self, **kwargs):
     #     dmat= kwargs['dmat']
@@ -262,48 +324,27 @@ class Tribal:
 
     
 
-    def run(self):
-        best_score = np.Inf
-        opt = []
-        for i in range(self.n_init):
-
-            #get an intiial TribalTree to start each run
-            start_tree = self.init()
-
-            opt.append(self.hill_climbing(start_tree))
-        
-        # best_trees = None
-        # best_score = np.Inf
-        # for trees in local_opt:
-        #     t = trees[0]
-        #     score = t.tree_score(self.alpha)
-        #     if score < best_score:
-        #         best_trees = trees
+    def run(self, n_init=1, alpha=0.9, k_max=1000, temp=50):
         best_tree = None
         best_score =np.Inf 
-        for trees in opt:
-            t = trees[0]
-            score = t.tree_score(self.alpha)
+        self.alpha  = alpha
+
+        print("start,k,cand_obj,curr_obj,best_obj")
+        start_trees = self.init(n_init)
+        for i,start_tree in enumerate(start_trees):
+
+    
+            # opt.append(self.greedy_hill_climbing(start_tree))
+            score, tree = self.simulated_annealing(start_tree, i, temp=temp, k_max=k_max)
+
             if score < best_score:
-                best_tree = t
-      
-        return best_tree
+                best_tree = tree
+                best_score = score
+        obj, pars_obj, iso_obj, labels, isotypes= self.parsimony(best_tree)
+        labels = {key : "".join(value) for key, value in labels.items()}
+        isotypes = {key: value[0] for key,value in isotypes.items()}
+        return best_tree, obj, pars_obj, iso_obj, labels, isotypes
         
-        # best_res = []
-        # for l in local_opt:
-        #     if l[0] == best_score:
-        #         best_res.append(l)
-
-        #     elif l[0] < best_score: 
-        #         best_res = [l]
-        #         best_score = l[0]
-         
-        
-        # for l in best_res:
-        #     self.print_tree(l)
-      
-        
-
 
 
 
@@ -324,9 +365,11 @@ if __name__ == "__main__":
         help="the id of the root sequence in the alignment")
     parser.add_argument("--init", choices=["random", "nj", "edmonds"], default="random")
     parser.add_argument("--n_init", type=int, default=1)
+    parser.add_argument("-k", "--kmax", type=int, default=1000)
+    parser.add_argument("--temp", type=float, default=50)
     parser.add_argument("-s", "--seed", type=int, default=1026)
     parser.add_argument("-o", "--output", type=str, help="outputfile of all best trees")
-    parser.add_argument("--alpha", type=float, default=0.5)
+    parser.add_argument("--alpha", type=float, default=0.9)
     parser.add_argument("-j", "--jump-prob", type=float, default=0.5)
     parser.add_argument( "--fasta", type=str, help="filename where reconstructed ancestral sequences should be saved as fasta file")
     parser.add_argument( "--sequences", type=str, help="filename where reconstructed ancestral sequences should be saved as csv file")
@@ -334,23 +377,25 @@ if __name__ == "__main__":
     parser.add_argument("--score",  type=str, help="filename of the objective function value objective function value")
     parser.add_argument("--iso_infer",  type=str, help="filename of the inferred isotypes for the internal nodes")
 
-
-    # args= parser.parse_args()
-    path = "/scratch/projects/tribal/benchmark_pipeline/sim_data/shm_sim/2.0/0.365/1"
-    args =parser.parse_args([
-        "-a", f"{path}/GCsim_dedup.fasta",
-        "-r", "naive",
-        "-t", f"{path}/GCsim_transition_matrix_geometric.txt",
-        "-i", f"{path}/GCsim_isotypes_geometric.csv",
-        "-o", "/scratch/projects/tribal/src/test/tribal.tree",
-        "--init", "nj",
-        "-s", "1",
-        "--sequences", "/scratch/projects/tribal/src/test/tribal.seq",
-        "--fasta","/scratch/projects/tribal/src/test/tribal.fasta",
-        "-n", "/scratch/projects/tribal/src/test/ednmonds.newick",
-        "--score","/scratch/projects/tribal/src/test/tribal.score",
-        "--iso_infer", "/scratch/projects/tribal/src/test/tribal.isotypes"
-    ])
+    args= parser.parse_args()
+    # path = "/scratch/projects/tribal/benchmark_pipeline/sim_data/shm_sim/2.0/0.365/1"
+    # args =parser.parse_args([
+    #     "-a", f"{path}/GCsim_dedup.fasta",
+    #     "-r", "naive",
+    #     "-t", f"{path}/GCsim_transition_matrix_geometric.txt",
+    #     "-i", f"{path}/GCsim_isotypes_geometric.csv",
+    #     "-o", "/scratch/projects/tribal/src/test/tribal.tree",
+    #     "--init", "random",
+    #     "--n_init", "1",
+    #     "-k", "26",
+    #     "--temp", "55",
+    #     "-s", "1",
+    #     "--sequences", "/scratch/projects/tribal/src/test/tribal.seq",
+    #     "--fasta","/scratch/projects/tribal/src/test/tribal.fasta",
+    #     "-n", "/scratch/projects/tribal/src/test/ednmonds.newick",
+    #     "--score","/scratch/projects/tribal/src/test/tribal.score",
+    #     "--iso_infer", "/scratch/projects/tribal/src/test/tribal.isotypes"
+    # ])
 
     
     # args= parser.parse_args([
@@ -380,16 +425,14 @@ if __name__ == "__main__":
         root= args.root,
         isotype_labels= isotypes_filt,
         seed = args.seed,
-        n_init= args.n_init,
         init = args.init,
         transmat = transMat,
-        alpha= args.alpha,
         jump_prob = args.jump_prob
 
     )
 
-    best_tree = tr.run()
-    labels, isotypes, seq_score, iso_score = best_tree.get_output()
+    best_tree, obj, par_obj, iso_obj, labels, isotypes = tr.run(args.n_init, args.alpha, 
+                                                                args.kmax, args.temp)
 
 
 
@@ -407,7 +450,7 @@ if __name__ == "__main__":
             file.write(newick)
     if args.score is not None:
         with open(args.score, 'w+') as file:
-            file.write(f"{seq_score},{iso_score}\n")
+            file.write(f"{args.alpha},{obj},{par_obj},{iso_obj}\n")
     if args.iso_infer is not None:
         ut.save_dict(isotypes, args.iso_infer)
 
@@ -415,27 +458,6 @@ if __name__ == "__main__":
   
 
 
-
-# s1 = ["a", "b", "c"]
-# s2 = ["c", "b", "c"]
-# s3 = ["a", "c", "c"]
-# s4 = ["a", "a", "a"]
-# alignment = {1: s1, 2: s2, 3 : s3, 4: s4}
-
-# seq0 = [ "A", "C", "G", "G"]
-# seq1 = [ "C", "T", "A", "G"]
-# seq2 = [ "C", "T", "G", "G"]
-# seq3 = [ "T", "T", "G", "A"]
-# seq4 = [ "A", "C", "A", "C"]
-# seq5 = [ "A", "C", "T", "C"]
-# isotype_labels = {1: 1, 2: 3, 3:2, 4:0, 5:1}
-# alignment = {0: seq0, 1: seq1, 2: seq2, 3: seq3, 4: seq4, 5: seq5}
-# ids = list(alignment.keys())
-# tr  = TreeBuild(alignment, isotype_labels)
-# opt_trees = tr.run()
-# print(len(opt_trees))
-# print(f" OPT Seq: {opt_seq_score} OPT Iso: {opt_iso_score}")
-# tr.print_tree()
 
 
 
