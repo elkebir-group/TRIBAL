c7d42c081bb3d2790b18debf769a82ea7a992526
Leah Weber <leahlw2@ada.cs.illinois.edu>
2022-12-21 17:00:39 -0600

    added data processing scripts

---
+++ real_data/scripts/mouse_transmat.py
@@ -0,0 +1,45 @@
+
+import argparse
+import numpy as np
+def gen_trans_mat(alpha,n ):
+    trans = np.zeros((n,n))
+    for i in range(n):
+        j_vals = np.arange(i+1, n)
+        for j in range(n):
+            if i > j:
+                trans[i,j] = 0
+            elif i ==j:
+                trans[i,j] = alpha
+            else:
+                trans[i,j] = (1-alpha)/j_vals.shape[0]
+    
+    trans[1,:] = 0
+    trans[1,1] = 1
+    trans[n-1, n-1] = 1
+    # print(trans.sum(axis=1))
+    return trans
+    
+    
+
+
+
+
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-a", "--alpha", type=float, default=0.75)
+    parser.add_argument("-n", "--nisotypes", type=int, default=8)
+    parser.add_argument("-o", "--outfile")
+    args = parser.parse_args([
+        "-o", "../mouse_transmat.txt"
+    ])
+
+    trans = gen_trans_mat(args.alpha, args.nisotypes)
+
+    if args.outfile is not None:
+        np.savetxt(args.outfile, trans)
+
+
+
