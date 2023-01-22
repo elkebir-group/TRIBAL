6e6a981906f582b68c520aed4ad406b709c7411c
Leah Weber <leahlw2@ada.cs.illinois.edu>
2023-01-22 13:19:48 -0600

    removed old code

--- src/tribal.py
+++ src/tribal.py
@@ -7,11 +7,9 @@ import pickle
 from multiprocessing import Pool
 from copy import deepcopy
 import utils as ut
-from tree_utils import TribalTree
 from ete3 import Tree
-from trans_matrix import TransMat
 from em_weight_matrix import EMProbs
-from lineage_tree import LineageTree, LineageForest
+from lineage_tree import LineageForest
 import init_transmat as tm
 
 from tribal_sub import TribalSub
@@ -108,45 +106,7 @@ class Tribal:
         return np.abs(old -new) < threshold
 
 
-    # def run(self):
-    #     best_tree_id_pointer = None
-    #     best_log_like_score = np.NINF
-    #     best_lin_forest = None
-    #     all_transmats ={}
-    #     all_state_probs = {}
-    #     best_trans =None
-    #     best_states = None
-    #     best_fit_scores = {}
-    #     log_like_scores = {}
-    #     for i in range(self.restarts):
 
-    #         print(f"\nStarting fitting phase for restart {i}.....")
-    #         fit_score, best_tree_ids, lin_forest, exp_log_like = self.fit()
-    #         print(f"\nFit Phase Complete for restart {i}!\nFit Score: {fit_score} Exp Log Like {exp_log_like}\n")
-    #         if exp_log_like > best_log_like_score:
-    #             best_log_like_score = exp_log_like
-
-    #             best_trans = self.transmat.copy()
-    #             best_states = self.state_probs.copy()
-    #             best_lin_forest = lin_forest
-    #             best_tree_id_pointer = best_tree_ids 
-    #             best_iter = i
-    #         best_fit_scores[i] = fit_score
-    #         log_like_scores[i] = exp_log_like
-
-    #         all_transmats[i] =self.transmat.copy()
-    #         all_state_probs[i] = self.state_probs.copy()
-    #         self.transmat = self.init_transmat
-    #         self.transmat =tm.add_noise(self.transmat, self.rng, mu=0.05, sigma=0.01, min_prob=0.01)
-
-
-            
-
-
-        # search_score, best_lin_trees =self.search(lin_forest)
-        # print(f"\nSearch Phase Complete!\nSearch Score: {search_score}")
-
-        # return best_fit_scores[best_iter], best_lin_forest, best_trans,best_states, all_transmats, all_state_probs, log_like_scores
 
 
     def run(self, nproc=1):
@@ -184,13 +144,6 @@ class Tribal:
             restart+= 1
 
 
-
-            
-
-
-        # search_score, best_lin_trees =self.search(lin_forest)
-        # print(f"\nSearch Phase Complete!\nSearch Score: {search_score}")
-
         return best_fit_scores[best_iter], best_lin_forest, best_trans,best_states, all_transmats, all_state_probs, log_like_scores
 
     
@@ -212,10 +165,6 @@ class Tribal:
         old_score = np.Inf
         for i in range(self.niterations):
             print(f"\nStarting Cycle {i}...")
-            # if i== 0:
-            #     transmat = None
-            # else:
-            #     transmat = self.transmat
 
             
             candidates = self.intialize_candidates(best_tree_ids)
@@ -223,7 +172,7 @@ class Tribal:
 
             print("\nFitting transition matrix...")
             cur_log_like, state_probs, transmat= EMProbs(lin_forest, transmat, self.states).fit(self.obs_states)
-            # self.transmat = new_tmat
+
 
          
             print(f"\nCycle {i} Complete: Current Score: {current_score} Previous Score: {old_score} Curr EM Log Like: {cur_log_like}")
@@ -290,17 +239,13 @@ def convert_to_nx(ete_tree, root):
         if node.is_root():
             root_name =node.name
 
-        # print(node.name)
+
         for c in node.children:
             if c.name == "":
                 c.name = str(internal_node)
                 internal_node += 1
                 internal_node_count += 1
-            # else:
-            #     print(c.name)
-            # if isinstance(c.name, str):
-            #     print(c.name)
-       
+    
 
             nx_tree.add_edge(node.name, c.name)
     
@@ -309,11 +254,6 @@ def convert_to_nx(ete_tree, root):
         nx_tree.remove_edge(root_name, root)
         nx_tree.add_edge(root, root_name)
       
-
-
-    
-
-
 
     return nx_tree
         
@@ -345,7 +285,7 @@ def create_input( path,  tree_path, clonotype, root, seq_fasta_fname, trees_fnam
     alignment = {key: list(value.strip()) for key,value in alignment.items()}
 
 
-    #only treat isotype file as a fasta file if .fasta is in the name, otherwise, we assme it is a csv file dictionary
+    #only treat isotype file as a fasta file if .fasta is in the name, otherwise, we assume it is a csv file dictionary
     if ".fasta" in iso_fname:
         isotypes = ut.read_fasta(iso_fname)
     else:
@@ -417,11 +357,9 @@ def create_trees(cand_fname):
             
 
             ete_tree = Tree(nw, format=0)
-            # ete_tree.name = args.root
-            # print(ete_tree)
+
             nx_tree= convert_to_nx(ete_tree, args.root)
-            # print(list(nx_tree.edges))
-            # ttree = TribalTree(nx_tree, root=args.root, is_rooted=True)
+          
             cand_trees.append(nx_tree)
         return cand_trees
 
@@ -433,43 +371,31 @@ def pickle_save(obj, fname):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
+    parser.add_argument("-p", "--path", type=str, required=True, help="path to the directory containing input files")
     parser.add_argument("-c", "--clonotypes", required=False, type=str,
-        help="filename with list of clonotype ids")
-    parser.add_argument("-p", "--path", type=str, required=True  )
+        help="filename with list of clonotype subdirectories that should be included in the inference. If not provided, scans provided path for all subdirectory names")
+    parser.add_argument("-e", "--encoding", type=str, help="text file isotype states listed in germline order")
+    parser.add_argument("--n_isotypes", type=int, default=7, help="the number of isotypes states to use if isotype encoding file is not provided and input isotypes are encoded numerically")
+    parser.add_argument( "--fasta", type=str, default= "concat.aln.fasta", help="filename of input MSA in fasta file")
     parser.add_argument("-i", "--isotypes",  type=str, default= "isotype.fasta",
         help="filename of isotype fasta file within each clonotype directory")
+    parser.add_argument("-j", "--jump_prob", type=float, default=0.25, help="for inititalization of transition matrix if not provided")
     parser.add_argument("-t", "--transmat", required=False, type=str,
-        help="filename of input transition matrix")
+        help="optional filename of input transition matrix for initialization")
     parser.add_argument("-r", "--root", required=False, default="naive",
         help="the common id of the root in all clonotypes")
-    parser.add_argument(  "--tree_path", type=str, required=True, help="path to directory where candidate trees are saved")
-
+    parser.add_argument( "--tree_path", type=str, required=True, help="path to directory where candidate trees are saved")
     parser.add_argument("--candidates", type=str, default="outtree", help="filename containing newick strings for candidate trees")
     parser.add_argument("--niter", type=int, help="max number of iterations in the fitting phase", default=10)
     parser.add_argument("--thresh", type=float, help="theshold for convergence in fitting phase" ,default=0.1)
     parser.add_argument("--mu", type=float, default=0.07, help="mean of gaussian white noise to add for distortion")
     parser.add_argument("--sigma", type=float, default=0.05, help="std of gaussian white noise to add for distortion")
-    parser.add_argument("--nworkers", type=int, default=1, help="number of workers to use in the event in multiple restarts")
-
-    parser.add_argument("--max_cand", type=int, default = 10,  help="max candidate tree size per clonotype")
+    parser.add_argument("--nworkers", type=int, default=2, help="number of workers to use in the event in multiple restarts")
+    parser.add_argument("--max_cand", type=int, default = 20,  help="max candidate tree size per clonotype")
     parser.add_argument("-s", "--seed", type=int, default=1026)
-    parser.add_argument("-e", "--encoding", type=str, help="text file isotype states listed in germline order")
-    parser.add_argument("--n_isotypes", type=int, default=7, help="the number of isotypes states to use if isotype encoding file is not providied")
-
     parser.add_argument("--alpha", type=float, default=0.9)
-    parser.add_argument("-j", "--jump_prob", type=float, default=0.25, help="for inititalization of transition matrix if not provided")
     parser.add_argument("--restarts",  type=int, default=1, help="number of restarts")
-    parser.add_argument("--trees_fname", type=str, )
     parser.add_argument("--mode", choices=["score", "refine", "search"], default="score")
-    
-    
-    
-    parser.add_argument( "--fasta", type=str, default= "concat.aln.fasta", help="filename of input MSA in fasta file")
-    parser
-    # parser.add_argument("--mode", type=str, choices=["fit", "search"],
-    #             help="fit only resolves polytomyies and does not perform tree moves, search will search tree space for the candidate set")
-    
-    
     parser.add_argument( "-o", "--outpath", type=str, help="path to directory where output files should be saved")
     parser.add_argument("--score", type=str, help="filename where the score file should be saved")
     parser.add_argument("--transmat_infer", type=str, help="filename where the inferred transition matrix should be saved")
@@ -478,40 +404,12 @@ if __name__ == "__main__":
     parser.add_argument("--diagram_pdf", type=str, help="filename where the pdf of transition matrix should be saved")
     parser.add_argument("--heatmap", type=str, help="filename where the heatmap pdf of transition matrix should be saved")
     parser.add_argument("--save_all_restarts", type=str, help="path where all restarts should be saved")
-
-    # parser.add_argument( "--sequences", type=bool, action="store_true", help="if ancestral sequences should be saved")
-    # parser.add_argument("-n", "--newick", type=bool, action="store_true",  help="if newick string should be saved")
-    # parser.add_argument("--score",  type=bool, action="store_true")
-    # parser.add_argument("--iso_infer", type=bool, action="store_true")
-
-    # parser.add_argument("--save_candidates", type=bool, action="store_true")
-    args= parser.parse_args()
+    args = parser.parse_args(None if sys.argv[1:] else ['-h'])
+  
+  
 
     print(f"jump prob: {args.jump_prob}")
-    # path = "/scratch/projects/tribal/real_data"
-    # dataset = "GCB_NP_2"
- 
 
-    # args =parser.parse_args([
-    #     "-c", f"{path}/{dataset}/clonotypes.txt",
-    #     "-p", f"{path}/{dataset}/input",
-    #     "-r", "naive",
-    #     "-j", "0.35",
-    #     "-e", f"{path}/mouse_isotype_encoding.txt",
-    #     "-s", "3",
-    #     # "--score", f"{path}/{dataset}/tribal/score.txt",
-    #     "-o", f"{path}/test",
-    #     "--alpha", "0.75",
-    #     "--max_cand", "5",
-    #     "--niter", "3",
-    #     "--thresh", "0.1",
-    #     "--tree_path", f"{path}/{dataset}/dnapars",
-    #     "--save_all_restarts", f"{path}/test",
-    #     "--diagram", f"{path}/test/state_diagram.png",
-    #     "--restarts", "3",
-    #     "--nworkers", "3"
-
-    # ])
 
     if args.encoding is not None:
         iso_encoding, start_iso, n_isotypes = create_isotype_encoding(args.encoding)
@@ -524,15 +422,12 @@ if __name__ == "__main__":
     
     
     if args.transmat is not None:
-    #     transmat= TransMat(n_isotypes=n_isotypes).fit(args.jump_prob)
-    # else:
+
         transmat = np.loadtxt(args.transmat)
     else:
         transmat= None
 
-    # if n_isotypes != transmat.shape[0]:
-    #     raise ValueError("Isotype states in transition matrix does not match the number of states in encoding file")
-    
+
     
     if args.clonotypes is not None:
         clonotypes = []
@@ -567,10 +462,9 @@ if __name__ == "__main__":
     
 
   
-    # obj_score, lin_forest, transmat, state_probs, all_transmats, all_state_probs, all_log_like = tr.run()
     obj_score, lin_forest, transmat, state_probs, all_transmats, all_state_probs, all_log_like = tr.run(args.nworkers)
 
-    # print(f"Tribal Object Score: {obj_score}")
+
     print("\nTRIBAL Complete!, saving results...")
 
     if args.score is not None:
@@ -592,7 +486,7 @@ if __name__ == "__main__":
 
     if args.outpath is not None:
         lin_forest.save_trees(args.outpath)
-        # save_results(args.outpath, lin_tree_dict, pngs=True, isotype_mapping=rev_encoding)
+ 
   
 
  
