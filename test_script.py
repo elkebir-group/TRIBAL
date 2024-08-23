32bd88c18050eb255419a9c659149ccade3a2e2c
Leah <lweber21@gmail.com>
2024-08-23 13:08:28 -0500

    renamed test script

--- test_script.py
+++ test_script.py
@@ -3,27 +3,20 @@ Vignette to demonstrate the capabilities of the tribal package.
 """
 
 from tribal.preprocess import preprocess
-from tribal import probabilites,df, roots
+from tribal import df, roots
 from tribal import Tribal
-
-from tribal import lineage_tree, lineage_tree_list
 
 
 if __name__ == "__main__":
 
 
-    print(lineage_tree)
-    print(lineage_tree_list)
-    print(len(lineage_tree_list))
-    print(probabilites.shape)
-  
-
             
     isotypes = ['IGHM', 'IGHG3', 'IGHG1', 'IGHA1','IGHG2','IGHG4','IGHE','IGHA2']
 
+    #test dnapars
     clonotypes, df_filt = preprocess(df, roots,isotypes, cores=3, verbose=True )
 
-
+    #test tribal
     tr = Tribal(n_isotypes=len(isotypes), verbose=True, restarts=1, niter=15)
             
     #run in refinement mode
